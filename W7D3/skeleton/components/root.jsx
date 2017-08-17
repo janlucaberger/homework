13763401b0d5835616b2896c00dbf02de96a99b1
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-17 08:45:11 -0400

    Add homework for W7D4

---
+++ W7D3/skeleton/components/root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import GiphysSearchContainer from './giphys_search_container';
+
+const Root = () =>{
+  return(
+    <div></div>
+  )
+}
+
+export default Root
